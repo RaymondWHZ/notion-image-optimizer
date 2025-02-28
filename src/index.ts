6528be6e99470a9a20ac16032febcef7b02104bc
RaymondWHZ <wuhaozhen1106@gmail.com>
2025-02-27 21:25:23 -0600

    feat: implement put images

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,37 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const getKeyFromNotionImageUrl = (url: string) => new URL(url).pathname.slice(1)
+
 export default {
 	async fetch(request, env): Promise<Response> {
 		// Parse request URL to get access to query string
 		let url = new URL(request.url)
 
 		if (request.method === 'PUT') {
-			// TODO update existing cache
-			return new Response('Not implemented', { status: 501 })
+			if (url.pathname !== '/') {
+				return new Response('Not found', { status: 404 })
+			}
+
+			const { notionImageUrls } = await request.json<{ notionImageUrls: string[] }>()
+			const newKeySet = new Set(notionImageUrls.map(getKeyFromNotionImageUrl))
+
+			const { objects } = await env.CACHED_NOTION_IMAGES_BUCKET.list()
+			const currentKeys = objects.map(_ => _.key)
+			const keysToDelete = currentKeys.filter(_ => !newKeySet.has(_))
+			const keysToCreate = notionImageUrls.filter(_ => !currentKeys.includes(_))
+
+			await Promise.all([
+				keysToDelete.length > 0 ? env.CACHED_NOTION_IMAGES_BUCKET.delete(keysToDelete) : Promise.resolve(),
+				...keysToCreate.map(async _ => {
+					const response = await fetch(_)
+					const key = getKeyFromNotionImageUrl(_)
+					const body = response.body
+					await env.CACHED_NOTION_IMAGES_BUCKET.put(key, body)
+				})
+			])
+
+			return new Response('OK')
 		} else if (request.method === 'GET') {
 			// Get object key
 			const key = url.pathname.slice(1)
